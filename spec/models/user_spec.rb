d0a157e189e10252beac11109ca86d5f010730db
Dmitri Severinov <dmitry.severinov@gmail.com>
2013-06-10 09:32:05 -0700

    Finishes chapter 6. User model added to the project

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,12 +12,19 @@
 require 'spec_helper'
 
 describe User do
-	before { @user = User.new(name: "Example User", email: "user@example.com") }
+	before { @user = User.new(name: "Example User", email: "user@example.com",
+								password: "foobar", password_confirmation: "foobar")}
 	subject { @user }
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:authenticate) }
+	
 	it { should be_valid}
-	  
+
+	#user name validations	
 	describe "when name is not present" do
 		before { @user.name = "" }
 		it { should_not be_valid }
@@ -28,6 +35,28 @@ describe User do
 		it { should_not be_valid }
 	end
 	
+	#password validations
+	describe "when password is not present" do
+		before { @user.password = @user.password_confirmation = "" }
+		it { should_not be_valid }
+	end
+	
+	describe "when password and password_confirmation do not match" do
+		before { @user.password_confirmation = "mismatch" }
+		it { should_not be_valid }
+	end
+	
+	describe "when password_confirmation is nil" do
+		before { @user.password_confirmation = nil }
+		it { should_not be_valid }
+	end
+	
+	describe "when password is too short" do
+		before { @user.password = @user.password_confirmation = "12345" }
+		it { should_not be_valid }
+	end
+	
+	#email validations
 	describe "when email address is already taken" do
 		before do
 			user_with_the_same_email = @user.dup
@@ -51,6 +80,34 @@ describe User do
 				@user.email = valid_email
 				@user.should be_valid
 			end
+		end
+	end
+	
+	describe "should be saved in downcase" do
+		let(:mixed_case_email) { "MixEd@CasE.EmaiL" }
+		
+		it "should be saved in lower case" do
+			@user.email = mixed_case_email
+			@user.save
+			@user.reload.email.should == mixed_case_email.downcase
+		end
+	end
+	
+	
+	
+	#authentication validation
+	describe "return value of authenticate method" do
+		before { @user.save }
+		let(:found_user) { User.find_by_email(@user.email) }
+		
+		describe "with valid password" do
+			it { should == found_user.authenticate(@user.password) }
+		end
+		
+		describe "with invalid password" do
+			let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+			it { should_not == user_for_invalid_password }
+			specify { user_for_invalid_password.should be_false }
 		end
 	end
 end
